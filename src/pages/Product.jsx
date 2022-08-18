732cdede684efa52f8ab295b3c5c68720e0087ad
Dogukan Caltekin <dcaltekin@gmail.com>
2022-08-18 21:41:30 +0300

    details-page-stars-color-update

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -53,7 +53,7 @@ function Product() {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
-                      className="w-4 h-4 text-red-500"
+                      className="w-4 h-4 text-yellow-500"
                       viewBox="0 0 24 24"
                     >
                       <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
@@ -64,7 +64,7 @@ function Product() {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
-                      className="w-4 h-4 text-red-500"
+                      className="w-4 h-4 text-yellow-500"
                       viewBox="0 0 24 24"
                     >
                       <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
@@ -75,7 +75,7 @@ function Product() {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
-                      className="w-4 h-4 text-red-500"
+                      className="w-4 h-4 text-yellow-500"
                       viewBox="0 0 24 24"
                     >
                       <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
@@ -86,7 +86,7 @@ function Product() {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
-                      className="w-4 h-4 text-red-500"
+                      className="w-4 h-4 text-yellow-500"
                       viewBox="0 0 24 24"
                     >
                       <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
@@ -97,12 +97,12 @@ function Product() {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth="2"
-                      className="w-4 h-4 text-red-500"
+                      className="w-4 h-4 text-yellow-500"
                       viewBox="0 0 24 24"
                     >
                       <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                     </svg>
-                    <span className="text-gray-600 ml-3">
+                    <span className="text-gray-400 ml-3">
                       {productData.rating.rate} Rate in{" "}
                       {productData.rating.count} Vote
                     </span>
